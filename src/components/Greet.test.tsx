6061d11d8aaab1d30f613a51c64230a0a47b41ee
Guilherme Ribeiro <xxguilhermesoaresxx@gmail.com>
2023-03-17 10:02:21 -0300

    Creating code coverage script

--- src/components/Greet.test.tsx
+++ src/components/Greet.test.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./Greet";
 
-it('should render correctly', () => {
-  render(<Greet/>);
-  const textElement = screen.getByText('Hello');
-  expect(textElement).toBeInTheDocument();
-});
-
-it('should render with a name', () => {
-  render(<Greet name='Guilherme'/>);
-  const textElement = screen.getByText('Hello Guilherme');
-  expect(textElement).toBeInTheDocument();
-});
+describe('Greet component', () => {
+  it('should render correctly', () => {
+    render(<Greet/>);
+    const textElement = screen.getByText('Hello');
+    expect(textElement).toBeInTheDocument();
+  });
+  
+  it('should render correctly with a custom name', () => {
+    render(<Greet name='Guilherme'/>);
+    const textElement = screen.getByText('Hello Guilherme');
+    expect(textElement).toBeInTheDocument();
+  });
+})
