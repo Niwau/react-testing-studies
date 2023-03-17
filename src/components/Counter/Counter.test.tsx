78c2a596d09b40e9d69439f5a4c2e18b25509067
Guilherme Ribeiro <xxguilhermesoaresxx@gmail.com>
2023-03-17 13:49:54 -0300

    Testing counter component with click

--- src/components/Counter/Counter.test.tsx
+++ src/components/Counter/Counter.test.tsx
@@ -1,13 +1,29 @@
 import { render, screen } from "@testing-library/react";
 import { Counter } from "./Counter";
+import user from '@testing-library/user-event';
 
 describe('Counter', () => {
   it('should render correctly', () => {
     render(<Counter/>);
     const counter = screen.getByRole('heading', { level: 1});
     expect(counter).toBeInTheDocument();
-
-    const incrementButton = screen.getByRole('button', { name: 'Increment'});
+    const incrementButton = screen.getByRole('button', { name: 'Increment' });
     expect(incrementButton).toBeInTheDocument();
   })
+
+  it('should render a count of 0', () => {
+    render(<Counter/>);
+    const counter = screen.getByRole('heading', { level: 1 });
+    expect(counter).toHaveTextContent('0');
+  })
+
+  it('should have a value of 1 after click', async () => {
+    user.setup();
+    render(<Counter/>);
+    const incrementButton = screen.getByRole('button', { name: 'Increment' });
+    const counter = screen.getByRole('heading', { level: 1 });
+    await user.click(incrementButton);
+    expect(counter).toHaveTextContent('1');
+  })
+
 })
